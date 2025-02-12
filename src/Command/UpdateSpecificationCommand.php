d3b64199365ef06a0fe45c06dc3d9bf9e2479287
Oleksandr Khudonohov <oleksandr@vespertool.com>
2025-02-12 10:39:08 +0100

    Replacing the slack specification file to use a local one

--- src/Command/UpdateSpecificationCommand.php
+++ src/Command/UpdateSpecificationCommand.php
@@ -42,14 +42,8 @@ class UpdateSpecificationCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        // download official Slack OpenAPI spec and save it (use github version to have response specs with examples)
-        $content = HttpClient::create()->request('GET', 'https://raw.githubusercontent.com/slackapi/slack-api-specs/master/web-api/slack_web_openapi_v2.json', [
-            'headers' => [
-                'Accept' => 'application/json',
-            ],
-        ])->getContent();
-        file_put_contents(__DIR__ . '/../../resources/slack-openapi.json', $content);
-        $output->writeln('<info>Downloaded and saved a new specification version</info>');
+        // Slack specification has not been updated on github for a while, so we use the local one
+        $content = file_get_contents(__DIR__ . '/../../src/slack-openapi.json');
 
         // sort the spec file
         $sorter = new JsonSorter();
